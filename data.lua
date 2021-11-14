bc95941801ac620198f9ea0f1afc82c63b31304b
Daniel Bergmann <brgmnn@users.noreply.github.com>
2021-11-14 00:19:42 -0800

    Add settings for unkillable vehicles and land mines

---
+++ data.lua
@@ -0,0 +1,15 @@
+local vehicleCategories = { "land-mine", "locomotive", "artillery-wagon", "cargo-wagon", "fluid-wagon" }
+
+for _, category in pairs(vehicleCategories) do
+  for _, vehicle in pairs(data.raw[category]) do
+    if (settings.startup["combat-tweaks--unkillable-"..category].value) then
+      vehicle.resistances = {}
+        for damageType, _ in pairs(data.raw["damage-type"]) do
+          vehicle.resistances[damageType] = {
+            type = damageType,
+            percent = 100
+          }
+        end
+    end
+  end
+end
---
+++ settings.lua
@@ -0,0 +1,42 @@
+data:extend({
+  {
+    type = "bool-setting",
+    name = "combat-tweaks--unkillable-land-mine",
+    setting_type = "startup",
+    default_value = false,
+    order = "1",
+    per_user = false
+  },
+  {
+    type = "bool-setting",
+    name = "combat-tweaks--unkillable-locomotive",
+    setting_type = "startup",
+    default_value = false,
+    order = "2",
+    per_user = false
+  },
+  {
+    type = "bool-setting",
+    name = "combat-tweaks--unkillable-artillery-wagon",
+    setting_type = "startup",
+    default_value = false,
+    order = "3",
+    per_user = false
+  },
+  {
+    type = "bool-setting",
+    name = "combat-tweaks--unkillable-cargo-wagon",
+    setting_type = "startup",
+    default_value = false,
+    order = "3",
+    per_user = false
+  },
+  {
+    type = "bool-setting",
+    name = "combat-tweaks--unkillable-fluid-wagon",
+    setting_type = "startup",
+    default_value = false,
+    order = "3",
+    per_user = false
+  },
+})
