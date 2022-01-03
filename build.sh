46c237ddb51a45173306cbd5f8dd9ab7c36b3dd8
Daniel Bergmann <brgmnn@users.noreply.github.com>
2022-01-03 09:00:35 -0800

    Update build script

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ version=`jq -r ".version" < info.json`
 path="combat-tweaks_$version"
 
 mkdir -p "build/$path"
-cp -r *.lua info.json locale "build/$path/"
+cp -r *.lua info.json graphics locale prototypes "build/$path/"
 cd build
 zip -r --compression-method=deflate "$path.zip" "$path"
